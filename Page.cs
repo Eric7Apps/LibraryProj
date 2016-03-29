8b8a6e9387c21ddbbd88efe9b1c5ac536b907150
Eric Chauvin <eric4apps@gmail.com>
2016-03-29 09:45:10 -0600

    Update Page.cs

--- Page.cs
+++ Page.cs
@@ -18,11 +18,13 @@ namespace DGOLibrary
   private MainForm MForm;
   private string URL = "";
   private string Title = ""; // The title given in the link tag.
-  private ECTime ContentsUpdated;
+  private ECTime ContentsUpdateTime;
   private string FileName = "";
   private int Index = 0;
-  private byte[] UTF8Contents;
+  private string SearchableContents = "";
+  // private byte[] UTF8Contents;
   private string RelativeURLBase = "";
+
 
   private Page()
     {
@@ -34,13 +36,44 @@ namespace DGOLibrary
     {
     MForm = UseForm;
 
-    ContentsUpdated = new ECTime();
+    ContentsUpdateTime = new ECTime();
     }
 
 
-  internal ulong GetContentsUpdatedIndex()
+  internal void AddStatusString( string InString, int MaxLength )
     {
-    return ContentsUpdated.GetIndex();
+    try
+    {
+    if( InString.Length > MaxLength )
+      InString = InString.Remove( MaxLength );
+
+    // In the future this will not run in the UI thread
+    // so it won't have direct access to MForm.
+    MForm.ShowStatus( InString );
+    }
+    catch( Exception ) // Except )
+      {
+      // MForm.ShowStatus( "Exception in " );
+      // MForm.ShowStatus( Except.Message );
+      }
+    }
+
+
+  internal bool GetIsCancelled()
+    {
+    if( MForm.GetIsClosing())
+      return true;
+
+    if( MForm.GetCancelled())
+      return true;
+
+    return false;
+    }
+
+
+  internal ulong GetContentsUpdateTimeIndex()
+    {
+    return ContentsUpdateTime.GetIndex();
     }
 
 
@@ -105,17 +138,20 @@ namespace DGOLibrary
 
 
 
-  internal bool UpdateFromFile( string UseTitle, string UseURL, string InFileName, bool SetTime, string RelativeURL )
+  internal void UpdateFromFile( string UseTitle, string UseURL, string InFileName, bool SetTime, string RelativeURL )
     {
-    if( MForm.PageList1 == null )
-      return false;
+    try
+    {
+    if( MForm.GetIsClosing() )
+      return;
 
+    SearchableContents = "";
 
     if( InFileName.Length < 3 )
       {
       MForm.ShowStatus( " " );
       MForm.ShowStatus( "No file name specified for: " + Title );
-      return false;
+      return;
       }
 
     if( URL.Length > 0 )
@@ -126,7 +162,7 @@ namespace DGOLibrary
         MForm.ShowStatus( "Title: " + Title );
         MForm.ShowStatus( "UseTitle: " + UseTitle );
         MForm.ShowStatus( "The URL being updated is different from the original one: " + URL );
-        return false;
+        return;
         }
       }
 
@@ -147,16 +183,35 @@ namespace DGOLibrary
 
     string FileContents = ReadFromTextFile( InFileName );
     if( FileContents == "" )
-      return false;
+      return;
+
+    if( FileContents.StartsWith( "%PDF-" ))
+      {
+      MForm.ShowStatus( "This is a PDF file." );
+      MForm.ShowStatus( "Deleting File: " + InFileName );
+      File.Delete( InFileName );
+      FileContents = "";
+      return;
+      }
 
     if( FileContents.Contains( "https://n26.network-auth.com/splash/" ))
       {
-      // Obviously this is not running on a real server here.
+      // Obviously this is not running on a real server
+      // when this happens.
       MForm.ShowStatus( "Cancelled because WiFi access needs OK." );
       MForm.ShowStatus( "Deleting File: " + InFileName );
       File.Delete( InFileName );
       MForm.SetCancelled();
-      return false;
+      return;
+      }
+
+    if( !FileContents.ToLower().Contains( "<html" ))
+      {
+      MForm.ShowStatus( "This is not an HTML file." );
+      MForm.ShowStatus( "Deleting File: " + InFileName );
+      File.Delete( InFileName );
+      FileContents = "";
+      return;
       }
 
     // Notice that a main page like the ones added in
@@ -171,103 +226,80 @@ namespace DGOLibrary
     // MForm.ShowStatus( "Updating: " + URL );
     // MForm.ShowStatus( "File name: " + FileName );
 
-    // This is meant to keep a historical index of articles
-    // so the advertising and other things in Javascript
-    // from years ago would be sent from a different
-    // server and it would be invalid.  A reference to
-    // a Javascript file on the server would be to a
-    // file that doesn't exist.  There are also 
-    // references to Facebook and Twitter that would
-    // not be valid.  Most of what's  between the
-    // comment tags is about the Javascript code.  It wouldn't
-    // make sense to index words for a search when
-    // they are in between comment tags.  It also
-    // makes it safer because there's no telling what
-    // an advertiser will include in their Javascript.
-    // It also makes the files smaller because there's
-    // a lot of Javascript and comments about the
-    // Javascript in every page.
-
     // GetCleanUnicodeString() was done in
     // ReadFromTextFile() for each line.
-    FileContents = Utility.SimplifyAndCleanCharacters( FileContents );
-
-    // RemoveFromStartToEnd doesn't account for nested 
-    // tags, but it should work for commented areas.
-    // The same comments and script are pretty much
-    // used in every file.
-    FileContents = Utility.RemoveFromStartToEnd( "<!--", "-->", FileContents );
-    FileContents = Utility.RemoveFromStartToEnd( "<script", "/script>", FileContents );
-    FileContents = Utility.RemoveFromStartToEnd( "<noscript", "/noscript>", FileContents );
-    FileContents = Utility.RemoveFromStartToEnd( "<link", "/>", FileContents );
-    FileContents = Utility.RemoveFromStartToEnd( "<meta", "/>", FileContents );
-    FileContents = Utility.RemoveFromStartToEnd( "<style", "/style>", FileContents );
-    // FileContents = Utility.RemoveFromStartToEnd( "<form", "/form>", FileContents );
-
-    // <div> tags are nested in all kinds of weird ways
-    // and they're not helpful.
-    // </p></div><div><p class="articleText">An
-    FileContents = Utility.RemoveFromStartToEnd( "<div", ">", FileContents );
-    FileContents = FileContents.Replace( "</div>", " " );
-
-    FileContents = FileContents.Replace( "”", "\"" );
-    FileContents = FileContents.Replace( "’", "'" );
-
-    if( SetTime )
-      ContentsUpdated.SetToNow();
 
     WriteToTextFile( FileContents );
-    MoveContentsToUTF8( FileContents );
+
+    // MoveContentsToUTF8( FileContents );
 
     string CleanContents = FileContents;
     FileContents = "";
 
-    // Until I come up with something better:
-    CleanContents = CleanContents.Replace( "<h1 style=\"text-align: center;margin: 10px 0 20px 0; font-size: 46px;\" >", "" );
+    if( SetTime )
+      ContentsUpdateTime.SetToNow();
 
-    CleanContents = CleanContents.Replace( "<br/>", " " );
-    CleanContents = CleanContents.Replace( "<br>", " " );
-    CleanContents = CleanContents.Replace( "<strong>", " " );
-    CleanContents = CleanContents.Replace( "</strong>", " " );
-    CleanContents = CleanContents.Replace( "<h1>", " " );
-    CleanContents = CleanContents.Replace( "<h2>", " " );
-    CleanContents = CleanContents.Replace( "<h3>", " " );
-    CleanContents = CleanContents.Replace( "<h4>", " " );
-    CleanContents = CleanContents.Replace( "<h5>", " " );
-    CleanContents = CleanContents.Replace( "<h6>", " " );
-    CleanContents = CleanContents.Replace( "</h1>", " " );
-    CleanContents = CleanContents.Replace( "</h2>", " " );
-    CleanContents = CleanContents.Replace( "</h3>", " " );
-    CleanContents = CleanContents.Replace( "</h4>", " " );
-    CleanContents = CleanContents.Replace( "</h5>", " " );
-    CleanContents = CleanContents.Replace( "</h6>", " " );
-    CleanContents = CleanContents.Replace( "<b>", " " );
-    CleanContents = CleanContents.Replace( "<i>", " " );
-    CleanContents = CleanContents.Replace( "</b>", " " );
-    CleanContents = CleanContents.Replace( "</i>", " " );
-    CleanContents = CleanContents.Replace( "<em>", " " );
-    CleanContents = CleanContents.Replace( "</em>", " " );
-    CleanContents = CleanContents.Replace( "&quot;", "\"" );
-    CleanContents = CleanContents.Replace( "&apos;", "'" );
-    CleanContents = CleanContents.Replace( "Capt. ", "Captain " );
-    CleanContents = CleanContents.Replace( "<center>", " " );
-    CleanContents = CleanContents.Replace( "</center>", " " );
-    // &eacute;   As in Resum&eacute;
+    // MForm.ShowStatus( "Code Comments:" );
+    string CommentLine = CleanContents.ToLower();
+    CommentLine = CommentLine.Replace( "\r", " " );
+    SortedDictionary<string, int> Lines = Utility.GetPatternsFromStartToEnd( "<!--", "-->", CommentLine );
+    if( Lines != null )
+      {
+      foreach( KeyValuePair<string, int> Kvp in Lines )
+        {
+        MForm.CodeCommentDictionary1.AddLine( Kvp.Key, RelativeURLBase );
+        // MForm.ShowStatus( Kvp.Key );
+        }
+      }
 
-    // <font>
-    // <center>
+    // MForm.ShowStatus( "Script:" );
+    string ScriptLine = CleanContents.ToLower();
+    ScriptLine = ScriptLine.Replace( "\r", " " );
+    Lines = Utility.GetPatternsFromStartToEnd( "<script", "/script>", ScriptLine );
+    if( Lines != null )
+      {
+      foreach( KeyValuePair<string, int> Kvp in Lines )
+        {
+        MForm.ScriptDictionary1.AddLine( Kvp.Key, RelativeURLBase );
+        // MForm.   AllWords.UpdateWord( Kvp.Key, URL );
+        // MForm.ShowStatus( Kvp.Key );
+        }
+      }
+
+    CleanContents = Utility.RemovePatternFromStartToEnd( "<!--", "-->", CleanContents );
+
+    // This will match <ScRipT.  It's not case sensitive.
+    CleanContents = Utility.RemovePatternFromStartToEnd( "<script", "/script>", CleanContents );
 
     // Parse what's in the tags recursively.
-    Tag OuterTag = new Tag( MForm, this, CleanContents, RelativeURLBase );
-    OuterTag.MakeContainedTags();
-    return true;
+    BasicTag BTag = new BasicTag( this, CleanContents, RelativeURLBase );
+    BTag.MakeContainedTags();
+    }
+    catch( Exception Except )
+      {
+      MForm.ShowStatus( "Exception in Page.UpdateFromFile()." );
+      MForm.ShowStatus( Except.Message );
+      }
     }
 
 
 
+
+  internal void AddLink( string Title, string LinkURL )
+    {
+    if( MForm.PageList1.ContainsURL( LinkURL ))
+      return;
+
+    // Get this new page:
+    if( MForm.GetURLMgrForm != null )
+      MForm.GetURLMgrForm.AddURLForm( Title, LinkURL, false, true, RelativeURLBase );
+
+    }
+
+
   private string MakeNewFileName()
     {
-    if( MForm.PageList1 == null )
+    if( MForm.GetIsClosing())
       return "";
 
     ECTime RightNow = new ECTime();
@@ -296,8 +328,7 @@ namespace DGOLibrary
     FileS += "T" + Ticks.ToString();
 
     // Using .txt instead of .htm because it's only
-    // used as text data here, and relative links won't
-    // work.  And also because of Javascript.
+    // used as text data here.
     FileS += ".txt";
     return Path + FileS;
     }
@@ -327,6 +358,9 @@ namespace DGOLibrary
 
   internal string ReadFromTextFile( string ReadFileName )
     {
+    if( MForm.GetIsClosing())
+      return "";
+
     try
     {
     if( !File.Exists( ReadFileName ))
@@ -349,7 +383,7 @@ namespace DGOLibrary
         if( Line == null )
           break;
 
-        Line = Utility.GetCleanUnicodeString( Line, 1000000 );
+        Line = Utility.GetCleanUnicodeString( Line, 1000000, true );
         if( Line.Length > 0 )
           SBuilder.Append( Line + "\r" );
 
@@ -407,7 +441,7 @@ namespace DGOLibrary
     {
     string Result = Title + "\t" +
            URL + "\t" +
-           ContentsUpdated.GetIndex().ToString() + "\t" +
+           ContentsUpdateTime.GetIndex().ToString() + "\t" +
            FileName + "\t" +
            Index.ToString() + "\t" +
            RelativeURLBase;
@@ -423,30 +457,25 @@ namespace DGOLibrary
     {
     string[] SplitS = InString.Split( new Char[] { '\t' } );
 
-    if( SplitS.Length < 5 )
+    if( SplitS.Length < 6 )
       return false;
 
-    Title = Utility.GetCleanUnicodeString( SplitS[0], 1000 );
-    URL = Utility.GetCleanUnicodeString( SplitS[1], 2000 );
-
-    URL = URL.Replace( "http://www.durangoherald.com//", "http://www.durangoherald.com/" );
-
-    if( URL.Contains( ".aspx?" ))
-      return false;
+    Title = Utility.GetCleanUnicodeString( SplitS[0], 1000, true );
+    URL = Utility.GetCleanUnicodeString( SplitS[1], 2000, true );
 
     if( URL.Contains( "durangoherald.com/#tab" ))
        return false;
 
     ulong TimeIndex = (ulong)Int64.Parse( SplitS[2] );
-    ContentsUpdated.SetFromIndex( TimeIndex );
-    int Year = ContentsUpdated.GetLocalYear();
+    ContentsUpdateTime.SetFromIndex( TimeIndex );
+    int Year = ContentsUpdateTime.GetLocalYear();
     if( Year < 2000 )
       return false;
 
     if( Year > 2099 )
       return false;
 
-    FileName = Utility.GetCleanUnicodeString( SplitS[3], 1000 );
+    FileName = Utility.GetCleanUnicodeString( SplitS[3], 2000, true );
 
     if( FileName.Length > 10 )
       {
@@ -464,21 +493,10 @@ namespace DGOLibrary
 
     Index = Int32.Parse( SplitS[4] );
 
-    if( SplitS.Length >= 6 )
-      RelativeURLBase = Utility.GetCleanUnicodeString( SplitS[5], 1000 );
-    else
-      RelativeURLBase = "http://www.durangoherald.com";
+    RelativeURLBase = Utility.GetCleanUnicodeString( SplitS[5], 1000, true );
 
-    if( RelativeURLBase.Length < 10 )
-      {
-      RelativeURLBase = "http://www.durangoherald.com";
-      }
-    else
-      {
-      if( RelativeURLBase[RelativeURLBase.Length - 1] == '/' )
-        RelativeURLBase = Utility.TruncateString( RelativeURLBase, RelativeURLBase.Length - 1 );
-
-      }
+    //   if( RelativeURLBase[RelativeURLBase.Length - 1] == '/' )
+    //     RelativeURLBase = Utility.TruncateString( RelativeURLBase, RelativeURLBase.Length - 1 );
 
     return true;
     }
@@ -491,7 +509,7 @@ namespace DGOLibrary
     }
 
 
-
+  /*
   internal void MoveContentsToUTF8( string FileContents )
     {
     if( FileContents == null )
@@ -519,6 +537,35 @@ namespace DGOLibrary
       throw( new Exception( "This should never happen in MoveContentsToUTF8()." ));
 
     }
+    */
+
+
+  internal void AddWords( SortedDictionary<string, int> WordsDictionary )
+    {
+    if( WordsDictionary == null )
+      return;
+
+    string URL = GetURL();
+    foreach( KeyValuePair<string, int> Kvp in WordsDictionary )
+      {
+      MForm.AllWords.UpdateWord( Kvp.Key, URL );
+      // MForm.ShowStatus( Kvp.Key );
+      }
+    }
+
+
+  internal void AddToSearchableContents( string InString )
+    {
+    SearchableContents = SearchableContents + " " + InString;
+    SearchableContents = SearchableContents.Trim();
+    }
+
+
+  internal string GetSearchableContents()
+    {
+    return SearchableContents;
+    }
+
 
 
 
